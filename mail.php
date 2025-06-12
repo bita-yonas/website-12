391b35ad11fc7c23e469df2a41035208741e55bf
bita-yonas <byonas21@stac.edu>
2025-06-12 19:26:07 -0400

    Updated project section and experience

--- mail.php
+++ mail.php
@@ -1,4 +1,7 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type");
 
 $method = $_SERVER['REQUEST_METHOD'];
 
